ed7948f0eea446de95128cd1f409ce5b25f07d81
hilla33 <you@example.comhilla33@wwu.edu.com>
2024-03-20 15:06:22 -0700

    updated readme, doing r&d

---
+++ main.c
@@ -0,0 +1,22 @@
+
+
+#include "definitions.h"
+
+#define MAIN
+#include "globals.h"
+
+
+
+
+
+int main(int argc, char** argv) {
+    //process flags and input
+
+    int opt;
+
+    while (opt = getopt(argc, argv))
+
+
+
+    return 0;
+}
