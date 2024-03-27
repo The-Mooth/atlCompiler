1c9df9f2d6dcd1ae45f6382e983eb610dd344ca3
hilla33 <you@example.comhilla33@wwu.edu.com>
2024-03-27 16:03:43 -0700

    added utility file, for common/misfit functions

---
+++ util.c
@@ -0,0 +1,14 @@
+#include "definitions.h"
+#include "globals.h"
+
+
+
+void debug_printf(char* line, ...) 
+{
+  if (debug) {
+    va_list args;
+    va_start(args, line); 
+    vprintf(line, args);
+    va_end(args);  
+    }
+}
