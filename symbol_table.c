9ce28ecafa70de469d11c7622d1804f2b620a090
hilla33 <you@example.comhilla33@wwu.edu.com>
2024-07-01 13:01:59 -0700

    symbol table initiated

--- symbol_table.c
+++ symbol_table.c
@@ -18,18 +18,38 @@ void init_symtab(char* progname) {
   type_desc_ptr int_desc = make_type_desc(tk_SCALAR, 4, 0, 0, NULL);
   id_info_ptr  int_symbol = make_id_info("int", ik_TYPE, int_desc, NULL);
 
-  
-
   type_desc_ptr bool_desc = make_type_desc(tk_SCALAR, 1, 0, 0, NULL);
-  id_info_ptr bool_desc = make_id_info("bool", ik_TYPE, bool_desc, NULL);
+  id_info_ptr bool_symbol = make_id_info("bool", ik_TYPE, bool_desc, NULL);
 
   type_desc_ptr string_desc = make_type_desc(tk_STRING, 0, 0, 0, NULL);
+  id_info_ptr string_symbol = make_id_info("string", ik_TYPE, string_desc, NULL);
 
+  type_desc_ptr err_desc = make_type_desc(tk_SCALAR, 0, 0, 0, NULL);
+  id_info_ptr err_symbol = make_id_info("error", ik_TYPE, err_desc, NULL);
+
+  insert_id(int_symbol);
+  insert_id(bool_symbol);
+  insert_id(string_symbol);
+  insert_id(err_symbol);
+  new_scope();
 
 }
 
-void insert_id(char* name, id_kind kind, type_desc_ptr type) {
+void insert_id(id_info_ptr id) {
   
+  debug_printf("Inserting id %s into scope %d\n", id->name, cur_scope);
+
+  if(kv_find(scope[cur_scope], id->name)) {
+    yyerror("Identifier %s already exists in scope.", id->name);
+    free(id);
+    return;
+  }
+  
+  if (kv_insert(scope[cur_scope], id->name, id) == 0) {
+    yyerror("Error inserting identifier %s into scope %d.", id->name, cur_scope);
+    free(id);
+  }
+
   return;
 }
 
@@ -55,12 +75,18 @@ void destroy_scope() {
   return;
 }
 
-
+//returns scope of id, or -2 if not found
+//remember, base scope is -1, so -2 is an error return
+int find_id(char* name) {
+  int i;
+  for (i = cur_scope; i >= -1; i--) {
+    if (kv_find(scope[i], name)) {
+      return i;
+    }
+  }
+  return -2;
+}
 /*needed functions:
-new_scope
-destroy_scope
-insert_id
-find_id
 print_scope
 print_all?
 
